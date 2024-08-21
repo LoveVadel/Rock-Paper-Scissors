0c2e26c5c89fd15949850979654c6da0520fae11
LoveVadel <loveosagie@hotmail.com>
2024-08-21 13:25:54 +0100

    Wrote a function for the game play
    wrote a function that takes in the human choice and compare it with computer choice to determine the winner, and also increments the round winner’s score.

--- main.js
+++ main.js
@@ -23,3 +23,45 @@ function getHumanChoice(){
     }
 }
 
+// Declare the players score variable
+let humanScore = 0;
+let computerScore = 0;
+
+// write a function for the game play that takes is the human choice and compare it with computer choice to determine the winner
+function playRound(humanChoice, computerChoice){
+
+    console.log(`YOU: ${humanChoice}`);
+    console.log(`COM: ${computerChoice}`)
+  
+      if(humanChoice === computerChoice){
+          return console.log("Its a tie")
+      }
+     else if(humanChoice === "rock" && computerChoice === "scissors"){
+      humanScore++;
+      return console.log("You win! Rock beats Scissors");
+     }
+     else if(humanChoice === "rock" && computerChoice === "paper"){
+      computerScore++
+      return console.log("You loss! paper beats Rock");
+     }
+     else if(humanChoice === "scissors" && computerChoice === "paper"){
+      humanScore++
+      return console.log("You win! Scissors beats Paper");
+     }
+     else if(humanChoice === "scissors" && computerChoice === "rock"){
+      computerScore++
+      return console.log("You loss! Rock beats Scissors");
+     }
+     else if(humanChoice === "paper" && computerChoice === "rock"){
+      humanScore++
+      return console.log("You Win! paper beats Rock");
+     }
+     else if(humanChoice === "paper" && computerChoice === "scissors"){
+      computerScore++
+      return console.log("You loss! Scissors beats paper");
+     }
+     else{
+      return console.log("Enter a valid choice")
+     }
+     
+  }
